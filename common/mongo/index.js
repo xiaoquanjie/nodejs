758bd898365930dd09b1557bde718ae719e666d3
xiaoquanjie <31240446+xiaoquanjie@users.noreply.github.com>
2022-05-19 16:54:47 +0800

    引入连接缓存

--- common/mongo/index.js
+++ common/mongo/index.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 
+// 默认的配置
 const defaultOption = {
     autoIndex: false,
     autoCreate: false,
@@ -7,8 +8,11 @@ const defaultOption = {
     connectTimeoutMS: 3000 // 不知为啥没效果
 };
 
+// 连接类型与连接
+const connTypeMap = {};
+
 // 创建一个mongo连接, 返回一个promise
-function connect(uri, opts) {
+function createConnection(uri, opts) {
     if (!opts) {
         opts = defaultOption;
     }
@@ -24,6 +28,55 @@ function connect(uri, opts) {
     });
 }
 
+// 设置连接选项
+function setConnectionOpts(connType, uri, opts) {
+    connTypeMap[connType] = { uri: uri, opts: opts, conn: undefined };
+}
+
+// 获取一个连接
+function getConnection(connType) {
+    return new Promise(function(resolve, reject) {
+        // 不认识的类型
+        if (!connTypeMap[connType]) {
+            return reject("ilegal connType:" + connType);
+        }
+        // 已有连接直接获取
+        if (connTypeMap[connType].conn) {
+            return resolve(connTypeMap[connType].conn);
+        }
+        // 创建连接
+        createConnection(
+            connTypeMap[connType].uri,
+            connTypeMap[connType].opts
+        )
+        .then(function(conn) {
+            conn.on('close', function() {
+                connTypeMap[connType].conn = undefined;
+            });
+            connTypeMap[connType].conn = conn;
+            resolve(conn);
+        })
+        .catch(function(err) {
+            reject(err);
+        });
+    });
+}
+
+// 关闭连接
+function closeConnection(connType) {
+    return new Promise(function(resolve, reject) {
+        if (!connTypeMap[connType]) {
+            return resolve();
+        }
+        if (!connTypeMap[connType].conn) {
+            return resolve();
+        }
+        var conn = connTypeMap[connType].conn;
+        connTypeMap[connType].conn = undefined;
+        conn.close().then(function() { resolve(); }).catch(function(err) { reject(err); });
+    });
+}
+
 function Schema(definition) {
     return new mongoose.Schema(definition);
 }
@@ -33,6 +86,8 @@ exports.model = function(db, name, definition) {
     return db.model(name, Schema(definition));
 }
 
-exports.connect = connect;
-
+exports.createConnection = createConnection;
+exports.setConnectionOpts = setConnectionOpts;
+exports.getConnection = getConnection;
+exports.closeConnection = closeConnection;
 exports.Schema = Schema;
